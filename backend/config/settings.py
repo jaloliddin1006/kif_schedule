dfb735023698dec70643de1b65fb19637bb348a0
oqdevpy <o.qosimov@tuit.uz>
2024-03-10 20:07:10 +0000

    update from server

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-v5yrpdk2q!bawhoqgu=jozu5w#-0by&se8um-)yd&n^gfw5bt^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["schedule.mamatmusayev.uz"]
 
 # CSRF_TRUSTED_ORIGINS = [
 #     'https://oqdevpy.jprq.live',
