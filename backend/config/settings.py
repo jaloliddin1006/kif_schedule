fb471982a6e6a1099729119a0d535f110a7f5c31
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2024-03-11 16:39:59 +0500

    some update setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'#Location of static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- desktop/main.py
+++ desktop/main.py
@@ -1,4 +1,4 @@
 import webview 
-web_url = 'http://127.0.0.1:8000/'
+web_url = 'https://schedule.mamatmusayev.uz/'
 webview.create_window("Desktop Web App", web_url, x=0, y=0, resizable=True, fullscreen=True )
 webview.start() 
