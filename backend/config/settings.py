0b1b0c013a75b1f28a8489100d2db81f5db17aec
Jaloliddin1006 <jmamatmusayev@gmail.com>
2024-09-07 09:43:44 -0700

    sd

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v5yrpdk2q!bawhoqgu=jozu5w#-0by&se8um-)yd&n^gfw5bt^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["schedule.mamatmusayev.uz", '127.0.0.1', '94.72.127.171']
 
